9ab6b42b07635252c716951dfe157de7126d9231
Zeyu Wang <wzy.franklin@gmail.com>
2016-12-07 16:56:13 +1100

    json

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,55 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+var bio = {
+    "name": "Zeyu Wang",
+    "role": "Front-end Developer",
+    "biopic": "images/fry.jpg ",
+    "welcome_message": "Welcome to the Fantastic World !",
+    "contact": {
+        "mobile number": "0450041077",
+        "email address": "wzy.franklin@gmail.com",
+        "github username": "ZeyuWangGit",
+        "location": "Acton, Act 2601"
+    },
+    "skill": ["HTML", "CSS", "JavaScript", "Java", "Python", "C", "C#", "Assembly"]
+};
+
+var education = {
+    "school": [{
+        "name": "ANU",
+        "city": "Canberra",
+        "degree": "master of computing",
+        "url": "www.anu.edu.au"
+    }],
+    "major": [{
+        "majors": "Computer Science",
+        "minors": "Artificial Intelligence"
+    }],
+    "onlineCourses": [{
+        "title": "Front-end Developer",
+        "school": "Udacity",
+        "degree date": "2017/01",
+        "url": "www.udacity.com"
+    }]
+};
+
+var work = {
+    "jobs": [{
+        "employer": "James",
+        "title": "Developer",
+        "location": "Canberra",
+        "dates": "5 days per week",
+        "descriptions": "nothing"
+    }]
+};
+var project = {
+    "projects": [{
+        "title": "Front-end Web Develop",
+        "dates": "5 days per week",
+        "descriptions": "nothing"
+    }],
+    "images": [{
+        "iamgeURL": "images/fry.jpg"
+    }]
+};
